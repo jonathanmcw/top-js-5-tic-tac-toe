d0c0791fbd902791af7413414ab9f388588f48a8
Jonathan Wong <jonathanmcw@gmail.com>
2025-01-18 00:01:48 +0800

    added html and css

--- script.js
+++ script.js
@@ -2,31 +2,29 @@
 function createUser(userName, num = 0) {
     const name = userName;
     const assignment = num;
-    const gender = "male";
+
     let score = 0;
+
     function giveScore() {
         return ++score;
     }
-    return {name, assignment, gender, giveScore};
+
+    return {name, assignment, giveScore};
 }
 
+// GameBoard
 function createGameBoard(p1, p2) {
     const grid =   [[0,0,0],
-                     [0,0,0],
-                     [0,0,0]]; // x: 0,1,2 y: 0,1,2
+                    [0,0,0],
+                    [0,0,0]]; // x: 0,1,2 y: 0,1,2
     
-    // const players = { p1:1, p2:-1};
-    // const players = { };
-    const players = [ p1, p2 ] ;
     // TODO: ERROR check if p1, p2 have attribution of .name, or is a player
-    // players[p1].assignment = 1; // O
-    // players[p2].assignment = -1; // X
+    const players = [ p1, p2 ] ;
     p1.assignment = 1;
     p2.assignment = -1;
 
     function playTurn( player, x, y ) {
         // TODO: ERR x, y range has to be 0, 1, 2
-        // grid[x][y] = players[player.name];
         grid[x][y] = player.assignment; 
         console.log(`${player.name} has moved to ${x} ${y}`);
         return {grid}
@@ -67,40 +65,45 @@ function createGameBoard(p1, p2) {
         }
     }
 
-    function show() {
-        for (let cell in grid) {
-            console.log(cell);
+    // Function to reset the game
+    function reset() {
+        console.log("Game reset");
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                grid[i][j] = 0;
+                grid[j][i] = 0;
+            }
         }
     }
 
-    return {grid, players, playTurn, show, checkWinner}
+    return {grid, players, playTurn, checkWinner, reset}
 }
 
 // Function to initialize the game
 function initializeGame() {
-    // Welcome instructions and enable selection of playing mode - P to P / P to C 
+
     // Setting up the game board
-    // console.log("Welcome to Tic Tac Toe!");
     const player1 = createUser("JW");
     const player2 = createUser("AI");
     const board = createGameBoard(player1, player2);
     
+    // Game loop
     let i = 0;
-    while ( i < 9 ) {
+    
+    outerLoop: while ( i < 9 ) {
         for ( let player of board.players ) {
             let [x, y] = prompt(`${player.name} it is ur turn to play, enter number "x-y"`).split('-');
-            // const  = str;
-            board.playTurn(player, x, y);
-            board.show();
-            board.checkWinner()
+            board.playTurn(player, x, y); // 0-0
             let winner = board.checkWinner(); 
             if (winner) {
-                alert(`${winner} win confirmed!`);
-                return winner;
+                break outerLoop;
             };
             i++;
         }
     }
+
+    board.reset();
+    console.log("Game Over");
 }
 
 // Function to handle player move
@@ -111,16 +114,11 @@ function initializeGame() {
 // }
 
 // Function to check for a winner
-function checkWinner() {
-    // At the end after each player has made a selection, check whether winning condition has been matched
-    // If any side makes connection ( could be a matrix, then the the )
-    console.log("Checking for a winner");
-}
-
-// Function to reset the game
-function resetGame() {
-    console.log("Game reset");
-}
+// function checkWinner() {
+//     // At the end after each player has made a selection, check whether winning condition has been matched
+//     // If any side makes connection ( could be a matrix, then the the )
+//     console.log("Checking for a winner");
+// }
 
 // Initialize the game when the script loads
 initializeGame();
